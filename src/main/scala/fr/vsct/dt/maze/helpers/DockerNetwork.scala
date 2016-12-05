5dbf8541a0081af23c40ff44f70b70ff7c15d9c0
francois_laroche <flaroche.prestataire@voyages-sncf.com>
2016-12-05 17:53:15 +0100

    Add default gateway as accept in iptable rules to allow remote control

--- src/main/scala/fr/vsct/dt/maze/helpers/DockerNetwork.scala
+++ src/main/scala/fr/vsct/dt/maze/helpers/DockerNetwork.scala
@@ -18,16 +18,18 @@ package fr.vsct.dt.maze.helpers
 
 import java.net.InetAddress
 
-import fr.vsct.dt.maze.topology.{Docker, DockerClusterNode, NodeGroup}
+import com.typesafe.scalalogging.StrictLogging
+import fr.vsct.dt.maze.topology.{ClusterNodeGroup, Docker, DockerClusterNode, NodeGroup}
 
 import collection.JavaConverters._
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Try
 
-object DockerNetwork {
+object DockerNetwork extends StrictLogging {
 
   val networkName = "technical-tests"
-  val subnet = "10.20.0.0/16"
+  val networkRange = "10.20.0"
+  val subnet = s"$networkRange.0/16"
 
   var nodesWithIpTablesModified: Seq[DockerClusterNode] = Seq()
 
@@ -50,6 +52,8 @@ object DockerNetwork {
 
   private val acceptAllRules = "iptables -F && iptables -P INPUT ACCEPT && iptables -P OUTPUT ACCEPT && iptables -P FORWARD ACCEPT"
   private val dropAllRules = s"iptables -A INPUT -d $subnet -j DROP && iptables -A OUTPUT -d $subnet -j DROP && iptables -A FORWARD -d $subnet -j DROP"
+  private val acceptGatewayRules =
+    s"iptables -A INPUT -d $networkRange.1 -j ACCEPT && iptables -A OUTPUT -d $networkRange.1 -j ACCEPT && iptables -A FORWARD -d $networkRange.1 -j ACCEPT"
   private val saveIpTablesRules = "/sbin/service iptables save"
 
   /* Each Seq[DockerClusterNode] provided will not see the other ones
@@ -93,7 +97,7 @@ object DockerNetwork {
         s"iptables -A INPUT -d $ip -j DROP && iptables -A OUTPUT -d $ip -j DROP && iptables -A FORWARD -d $ip -j DROP"
       }.mkString(" && ")
 
-      val command: Seq[String] = Seq("/bin/bash", "-c", s"$acceptAllRules && $commands && $saveIpTablesRules")
+      val command: Seq[String] = Seq("/bin/bash", "-c", s"$acceptAllRules && $acceptGatewayRules && $commands && $saveIpTablesRules")
       Docker.executionOnContainer(container, command: _*).execute()
     }
 
@@ -102,13 +106,13 @@ object DockerNetwork {
 
   /* Each provided Seq[DockerClusterNode] will be isolated from the rest of the world
      The main difference with split is that nodes not included in such Seq will not see the isolated nodes
-     Note that network traffic comming from outside the subnet wil still reach these nodes, allowing controll from outside
+     Note that network traffic coming from outside the subnet wil still reach these nodes, allowing control from outside
    */
-  def isolate(partitions: Seq[DockerClusterNode]*): Unit = {
+  def isolate(partitions: ClusterNodeGroup[DockerClusterNode]*): Unit = {
 
     // This only works in privileged mode.
     // If ip is a node from the same partition, in order to change the firewall rules, we want to execute (network is the subnet) :
-    // iptables -F (flush the iptables rules)
+    // iptables -F (flush the ip-tables rules)
     // iptables -P INPUT ACCEPT (by default, accept)
     // iptables -P FORWARD ACCEPT
     // iptables -P OUTPUT ACCEPT
@@ -120,35 +124,37 @@ object DockerNetwork {
     // iptables -A FORWARD -d <network> -j DROP
     // /sbin/service iptables save
 
-    val nodesAndTheirBuddiesIP: Map[String, Seq[String]] = (for {
+    val nodesAndTheirBuddiesIP: Map[DockerClusterNode, Seq[String]] = (for {
       partition <- partitions
-      container <- partition
-      otherContainer <- partition
-    } yield (container.containerId, otherContainer.containerId)
+      container <- partition.nodes
+      otherContainer <- partition.nodes
+    } yield (container, otherContainer.containerId)
       ).groupBy(_._1)
     // groupBy will return a Map[String, Set[(String, String)]], transform it to a Map[String, Set[String]]
      .map(tuple => (tuple._1, tuple._2.map(tuple2 => Docker.containerInfo(tuple2._2).getNetworkSettings.getNetworks.get(networkName).getIpAddress)))
 
-    nodesAndTheirBuddiesIP.foreach( entry => {
-      val containerId = entry._1
+    nodesAndTheirBuddiesIP.foreach ( entry => {
+      val container = entry._1
       val ips = entry._2
 
       val commands = ips.map { ip =>
         s"iptables -A INPUT -d $ip -j ACCEPT && iptables -A OUTPUT -d $ip -j ACCEPT && iptables -A FORWARD -d $ip -j ACCEPT"
       }.mkString(" && ")
 
-      val command = Seq("/bin/bash", "-c", s"$acceptAllRules && $commands && $dropAllRules && $saveIpTablesRules")
-      Docker.executionOnContainer(containerId, command: _*).execute()
+      val command = Seq("/bin/bash", "-c", s"$acceptAllRules && $acceptGatewayRules && $commands && $dropAllRules && $saveIpTablesRules")
+      logger.debug(s"Isolating node ${container.hostname} to only communicate with ${ips.mkString(", ")}")
+      Docker.executionOnContainer(container.containerId, command: _*).execute()
     })
 
-    nodesWithIpTablesModified ++= partitions.flatten
+    nodesWithIpTablesModified ++= partitions.flatMap(_.nodes)
   }
 
   /* ! This is supposed to work only with DockerNodes, it will fail at runtime otherwise ! */
   def cancelIsolation(): Unit = {
-    /* Filter with existing containers, this avoids failed cancelation or forgotten ones to break this method because of no more existing containers id */
-    nodesWithIpTablesModified.filter(Docker.listContainers().contains(_)).foreach { c =>
-      Docker.executionOnContainer(c.containerId, "/bin/bash", "-c", s"$acceptAllRules && $saveIpTablesRules").execute()
+    /* Filter with existing containers, this avoids failed cancellation or forgotten ones to break this method because of no more existing containers id */
+    val runningIds = Docker.listContainers().map(_.getId)
+    nodesWithIpTablesModified.map(_.containerId).filter(runningIds.contains(_)).foreach { id =>
+      Docker.executionOnContainer(id, "/bin/bash", "-c", s"$acceptAllRules && $saveIpTablesRules").execute()
     }
     nodesWithIpTablesModified = Seq()
   }
