a756ca2d9d507616e35f2dda72b79c219a497215
francois <flaroche@gmail.com>
2016-12-13 00:14:15 +0100

    Simplify HostNames object

--- src/main/scala/fr/vsct/dt/maze/topology/HostNames.scala
+++ src/main/scala/fr/vsct/dt/maze/topology/HostNames.scala
@@ -19,17 +19,12 @@ package fr.vsct.dt.maze.topology
 object HostNames {
 
   /* Stores current indexes for hostname. Useful to avoid hostname collision when the hostname is user predefined */
-  var hostnameIndexes: Map[String, Int] = Map()
+  private var hostnameIndexes: Map[String, Int] = Map()
 
   def getNextIndex(hostName: String): Int = {
-    if (hostnameIndexes contains hostName) {
-      val index = hostnameIndexes get hostName
-      hostnameIndexes += (hostName -> (index.get + 1))
-      index.get + 1
-    } else {
-      hostnameIndexes += (hostName -> 0)
-      0
-    }
+    val index = hostnameIndexes.getOrElse(hostName, -1)
+    hostnameIndexes += (hostName -> (index + 1))
+    index + 1
   }
 
 }
